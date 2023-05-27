e2d8399d9c65c04687109e9022d9597ffe6b9ab9
lomowang <frees1102@gmail.com>
2023-05-28 00:03:02 +0800

    20230528

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,78 @@
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer"
+import Home from "./pages/home/Home"
+import Gigs from "./pages/gigs/Gigs"
+import Gig from "./pages/gig/Gig"
+import Add from "./pages/add/Add"
+import Orders from "./pages/orders/Order"
+import Messages from "./pages/messgaes/Messages"
+import Message  from "./pages/messgage/Message";
+import MyGigs from "./pages/myGigs/Mygigs"
+import{
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+import"./app.scss"
+
 function App() {
+
+  const Layout = () =>{
+    return(
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children:[
+        {
+          path:"/",
+          element:<Home/>,
+        },
+        {
+          path:"/gigs",
+          element:<Gigs/>,
+        },
+        {
+          path:"/gig/:id",
+          element:<Gig/>,
+        },
+        {
+          path:"/orders",
+          element:<Orders/>,
+        },
+        {
+          path:"/mygigs",
+          element:<MyGigs/>,
+        },
+        {
+          path:"/add",
+          element:<Add/>,
+        },
+        {
+          path:"/messages",
+          element:<Messages/>,
+        },
+        {
+          path:"/message/:id",
+          element:<Message/>,
+        },
+      ]
+    },
+  ]);
 
   return (
     <div>
-      Hello world!
+    <RouterProvider router={router} />
     </div>
   )
 }
---
+++ src/pages/myGigs/Mygigs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Mygigs.scss"
+
+const Mygigs = () => {
+  return (
+    <div>Mygigs</div>
+  )
+}
+
+export default Mygigs
